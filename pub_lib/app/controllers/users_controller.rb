7892223b63f3f87966d4f3130ce9cacd5822210a
aapiane09 <aapiane09@gmail.com>
2017-01-27 16:32:26 -0800

    Adds login/logout functionality

--- pub_lib/app/controllers/users_controller.rb
+++ pub_lib/app/controllers/users_controller.rb
@@ -14,9 +14,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    redirect_to root_path
+    login(@user) # <-- log the user in
+    redirect_to @user # <-- go to show
   end
-
 
   private
 
