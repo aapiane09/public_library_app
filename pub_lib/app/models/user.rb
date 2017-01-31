9c9ae89232de232825e96ff6ebe39af13b0a0cdb
aapiane09 <aapiane09@gmail.com>
2017-01-30 22:40:45 -0800

    Added user model and created create functionality

--- pub_lib/app/models/user.rb
+++ pub_lib/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  
   has_many :library_users, dependent: :destroy
   has_many :libraries, through: :library_users
 
--- second_pub_lib/config/routes.rb
+++ second_pub_lib/config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
 
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get 'users/:id', to: 'users#show'
 end
